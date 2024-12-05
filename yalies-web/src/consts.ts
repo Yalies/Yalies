e351eb4cd7dc7f228d66ea930f4f52512b53983f
Eric Yoon <emjyoon@gmail.com>
2024-12-04 20:22:33 -0500

    oops

--- yalies-web/src/consts.ts
+++ yalies-web/src/consts.ts
@@ -1,3 +1,3 @@
 export const MOBILE_WDITH = 700;
 
-export const isMobile = () => window.innerWidth < MOBILE_WDITH;
+export const isMobile = () => typeof(window) !== "undefined" && window.innerWidth < MOBILE_WDITH;
